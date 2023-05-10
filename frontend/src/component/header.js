78b638409f68ee818e83df274d2b5297fb0b9a07
Mwadali <wellingtonmwadali@gmail.com>
2023-05-10 12:37:14 +0300

    created a logo

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -3,11 +3,12 @@ import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
-    <header className=''>
+    /*logo*/
+    <header className='fixed shadow-md w-full h-12 px-2 md:px-4'>
       {/*for desktop view */}
-      <div className=''>
-        <div className=''>
-          <img src={logo}/>
+      <div className='flex items-center'>
+        <div className='h-12'>
+          <img src={logo} className = 'h-full'/>
         </div>
       </div>
       {/*for mobile view*/}
