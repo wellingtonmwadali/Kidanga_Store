0bbb87a9c77ba6d93bdbd4ad948762c88a4e3bac
Mwadali <wellingtonmwadali@gmail.com>
2023-05-10 13:58:48 +0300

    added react routers

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -7,9 +8,11 @@ const Header = () => {
     <header className='fixed shadow-md w-full h-12 px-2 md:px-4'>
       {/*for desktop view */}
       <div className='flex items-center'>
-        <div className='h-12'>
+       <Link to={""}>
+       <div className='h-12'>
           <img src={logo} className = 'h-full'/>
         </div>
+       </Link>
       </div>
       {/*for mobile view*/}
     </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from  'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = '/' Element = {<App/>}>
+
+    </Route>
+  )
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router = {router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
