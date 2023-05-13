10de1576eae600411b5cb701b07e3136d68afef0
Mwadali <wellingtonmwadali@gmail.com>
2023-05-13 12:37:29 +0300

    created nav links

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -15,11 +15,15 @@ const Header = () => {
         </div>
        </Link>
        {/*icons*/}
-       <div className=''>
-        <nav>
+       <div className='flex items-center gap-3 md:gap-6'>
+        <nav className='flex gap-3 md:gap-6 text-base md:text-lg'>
+          <Link to = {""}>Home</Link>
+          <Link to = {"menu"}>Menu</Link>
+          <Link to = {"about"}>About</Link>
+          <Link to = {"contact"}>Contact</Link>
         </nav>
-        <div className=''><BsCart4/></div>
-        <div className=''><FaRegUser/></div>
+        <div className='text-2xl'><BsCart4/></div>
+        <div className='text-2xl'><FaRegUser/></div>
        </div>
       </div>
       {/*for mobile view*/}
