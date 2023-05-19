8a68ebf64615b5afbd39f5a6a20cb794c21e6dce
Mwadali <wellingtonmwadali@gmail.com>
2023-05-19 13:15:38 +0300

    signup gif\

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import loginAnimation from '../assets/login-animation.gif'
 
 const Signup = () => {
   return (
     <div className='p-3 md:p-4'>
         <div className='w-full max-w-sm bg-white m-auto'>
-            <h1 className='text-center'>Signup</h1>
+            {/*<h1 className='text-center text-2xl'>Signup</h1>*/}
+            <div className=''>
+              <img src={loginAnimation} className=''/>
+            </div>
         </div>
     </div>
   )
