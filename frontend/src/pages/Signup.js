80fdc4c20a21cb2de28d172765dedb6dd091ddfe
Mwadali <wellingtonmwadali@gmail.com>
2023-05-22 12:34:15 +0300

    handle submit on signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -6,30 +6,42 @@ import { Link } from 'react-router-dom';
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const handleShowPassword = () => {
-    setShowPassword(preve => !preve)
-  }
+    setShowPassword((preve) => !preve);
+  };
   const [data, setData] = useState({
-    firstName : "",
+    firstName: "",
     lastName: "",
-    email : "",
-    password : "",
-    confirmPassword : "",
-  })
-  console.log(data)
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log(data);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const handleShowConfirmPassword = () => {
-    setShowConfirmPassword(preve => !preve)
-  }
+    setShowConfirmPassword((preve) => !preve);
+  };
   const handleOnChange = (e) => {
-
-    const {name,value} = e.target;
+    const { name, value } = e.target;
     setData((preve) => {
-      return{
-          ...preve,
-        [name]: value
+      return {
+        ...preve,
+        [name]: value,
       };
     });
   };
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const {firstName,lastName,email,password,confirmPassword} = data
+    if (firstName && lastName && email && password && confirmPassword)
+    if(password === confirmPassword)
+    alert("success")
+    else{
+      alert("password and confirm password not equal")
+    }
+    else{
+      alert("please enter the required fields")
+    }
+  }
   return (
     <div className="p-3 md:p-4">
       <div className="w-full max-w-sm bg-white m-auto flex items-center flex-col p-4">
@@ -38,64 +50,78 @@ const Signup = () => {
           {/*animated signup gif in form*/}
           <img src={loginAnimation} className="w-full" />
         </div>
-        <form className="w-full py-3 flex flex-col">
+        <form className="w-full py-3 flex flex-col"onSubmit={handleSubmit}>
           <label htmlFor="firstName">First Name</label>
           <input
             type={"text"}
             id="firstName"
             name="firstName"
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-red-300"
-            value= {data.firstName}
-            onChange= {handleOnChange}
+            value={data.firstName}
+            onChange={handleOnChange}
           />
-          <label htmlFor='lastName'>Last Name</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
             type={"text"}
             id="lastName"
             name="lastName"
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-red-300"
-            value= {data.lastName}
-            onChange= {handleOnChange}
+            value={data.lastName}
+            onChange={handleOnChange}
           />
-          <label htmlFor='email'>Email</label>
+          <label htmlFor="email">Email</label>
           <input
-          type={"email"}
-          id="email"
-          name="email"
-          className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-red-300"
-          value= {data.email}
-          onChange= {handleOnChange}
+            type={"email"}
+            id="email"
+            name="email"
+            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-red-300"
+            value={data.email}
+            onChange={handleOnChange}
           />
-          <label htmlFor='password'>Password</label>
-          <div className='flex px-2 py-1 rounded bg-slate-200 mt-1 mb-2 focus-within:outline focus-within:outline-red-300'>
-          <input
-          type={showPassword ? "text" : "password"}
-          id="password"
-          name="password"
-          className=" w-full bg-slate-200 border-none outline-none"
-          value= {data.password}
-          onChange= {handleOnChange}
-          />
-          <span className='flex cursor-pointer 'onClick = {handleShowPassword}>{showPassword ? <BiShow/> : <BiHide/>}</span>
+          <label htmlFor="password">Password</label>
+          <div className="flex px-2 py-1 rounded bg-slate-200 mt-1 mb-2 focus-within:outline focus-within:outline-red-300">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className=" w-full bg-slate-200 border-none outline-none"
+              value={data.password}
+              onChange={handleOnChange}
+            />
+            <span className="flex cursor-pointer " onClick={handleShowPassword}>
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
           </div>
-          <label  htmlFor='confirmPassword'>Confirm Password</label>
-          <div className='flex px-2 py-1 rounded bg-slate-200 mt-1 mb-2 focus-within:outline focus-within:outline-red-300'>
-          <input
-          type={showConfirmPassword  ? "text" : "password"}
-          id="confirmPassword"
-          name="confirmPassword"
-          className=" w-full bg-slate-200 border-none outline-none"
-          value= {data.confirmPassword}
-          onChange= {handleOnChange}
-          />
-          <span className='flex cursor-pointer 'onClick = {handleShowConfirmPassword }>{showPassword ? <BiShow/> : <BiHide/>}</span>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <div className="flex px-2 py-1 rounded bg-slate-200 mt-1 mb-2 focus-within:outline focus-within:outline-red-300">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirmPassword"
+              name="confirmPassword"
+              className=" w-full bg-slate-200 border-none outline-none"
+              value={data.confirmPassword}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex cursor-pointer "
+              onClick={handleShowConfirmPassword}
+            >
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
           </div>
-          <button className='w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer text-white text-xl font-medium text-center py-1 rounded-full'>Signup</button>
+          <button className="w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer text-white text-xl font-medium text-center py-1 rounded-full">
+            Signup
+          </button>
         </form>
-        <p className='text-sm mt-1'>Already have account?<Link to={"login"}className='font-medium text-red-600 underline'>Login</Link></p>
+        <p className="text-sm mt-1">
+          Already have account?
+          <Link to={"login"} className="font-medium text-red-600 underline">
+            Login
+          </Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default Signup
