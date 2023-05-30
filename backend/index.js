942385e0721262f75592a46c37a29c1f1ee61e15
Mwadali <wellingtonmwadali@gmail.com>
2023-05-30 23:40:35 +0300

    enabled upload profile pic to server

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit : "10mb"}))
 
 const PORT = process.env.PORT || 8080
 
