732a5e8e4ec5bbf7b4d121b8b610c927b51e4706
Mwadali <wellingtonmwadali@gmail.com>
2023-05-31 15:16:37 +0300

    comments

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
+//imports express, cors, mongodb as mongoose
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose");
 
+//express api
 const app = express()
 app.use(cors())
 app.use(express.json({limit : "10mb"}))
 
 const PORT = process.env.PORT || 8080
 
-
+//fetch javascript api
 app.get("/",(req, res)=>{
     res.send("Server is running")
 })
