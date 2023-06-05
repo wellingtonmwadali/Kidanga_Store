b9f31248fcf46d883e9b0cf1b6d7ac4b727028f8
Mwadali <wellingtonmwadali@gmail.com>
2023-06-05 12:57:00 +0300

    installed dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 //imports express, cors, mongodb as mongoose
 const express = require("express")
 const cors = require("cors")
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
 //express api
 const app = express()
@@ -10,6 +11,9 @@ app.use(express.json({limit : "10mb"}))
 
 const PORT = process.env.PORT || 8080
 //mongodb connection 
+console.log(process.env.MONGODB_URL)
+mongoose.connect("")
+
 
 
 //fetch javascript api
