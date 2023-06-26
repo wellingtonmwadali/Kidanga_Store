98b6f1e487732cf96db5d9426851ec1ae32b32c2
Mwadali <wellingtonmwadali@gmail.com>
2023-06-26 23:58:23 +0300

    save product to database

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,15 +1,13 @@
-import React, {useState} from 'react'
-import loginAnimation from '../assets/login-animation.gif'
-import {BiShow,BiHide} from 'react-icons/bi';
-import { Link, useNavigate } from 'react-router-dom'; 
-import {imagetoBase64} from '../utility/imagetoBase64'; 
-import { toast } from 'react-hot-toast';
+import React, { useState } from "react";
+import loginAnimation from "../assets/login-animation.gif";
+import { BiShow, BiHide } from "react-icons/bi";
+import { Link, useNavigate } from "react-router-dom";
+import { imagetoBase64 } from "../utility/imagetoBase64";
+import { toast } from "react-hot-toast";
 
-
-
-
-
-{/*show password/hide password*/}
+{
+  /*show password/hide password*/
+}
 const Signup = () => {
   //handle show password
   const navigate = useNavigate();
@@ -24,11 +22,11 @@ const Signup = () => {
     email: "",
     password: "",
     confirmPassword: "",
-    image : "",
+    image: "",
   });
 
   console.log(data);
-//handle confirm password to be visible
+  //handle confirm password to be visible
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const handleShowConfirmPassword = () => {
     setShowConfirmPassword((preve) => !preve);
@@ -42,24 +40,28 @@ const Signup = () => {
       };
     });
   };
-  {/**handle uploading profile pic */}
-  const handleUploadProfilePhoto = async(e) => {
-    const data = await imagetoBase64(e.target.files[0])
-    console.log(data)
-
-    setData((preve) =>{
-      return{
-        ...preve,
-        image : data
-      }
-    })
+  {
+    /**handle uploading profile pic */
   }
-  {/**handle submit for signup to check for eligibility
-    **check also if password is same to confirm password
-    **fetch api
-  */}
-  console.log(process.env.REACT_APP_SERVER_DOMAIN)
-  const handleSubmit = async(e) =>{
+  const handleUploadProfilePhoto = async (e) => {
+    const data = await imagetoBase64(e.target.files[0]);
+    console.log(data);
+
+    setData((preve) => {
+      return {
+        ...preve,
+        image: data,
+      };
+    });
+  };
+  {
+    /**handle submit for signup to check for eligibility
+     **check also if password is same to confirm password
+     **fetch api
+     */
+  }
+  console.log(process.env.REACT_APP_SERVER_DOMAIN);
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const { firstName, lastName, email, password, confirmPassword } = data;
     if (firstName && lastName && email && password && confirmPassword)
@@ -79,7 +81,7 @@ const Signup = () => {
         console.log(dataRes);
         //alert(dataRes.message)
         toast(dataRes.message);
-//navigate to login after signup
+        //navigate to login after signup
         if (dataRes.alert) {
           navigate("/login");
         }
@@ -89,24 +91,32 @@ const Signup = () => {
     else {
       alert("please enter the required fields");
     }
-  }
+  };
   return (
     <div className="p-3 md:p-4">
       <div className="w-full max-w-sm bg-white m-auto flex items-center flex-col p-4">
         {/*<h1 className='text-center text-2xl'>Signup</h1>*/}
         <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md relative ">
           {/*animated signup gif in form*/}
-          <img src={data.image ? data.image : loginAnimation} className="w-full h-full" />
+          <img
+            src={data.image ? data.image : loginAnimation}
+            className="w-full h-full"
+          />
           {/* upload profile pic */}
           <label htmlFor="profilePhoto">
-          <div className="absolute bottom-0 h-1/3 bg-slate-500 bg-opacity-25 w-full text-center cursor-pointer">
-            <p className="text-sm p-1 text-white">upload</p>
-          </div>
-          <input type={"file"} id= "profilePhoto" className="hidden" onChange= {handleUploadProfilePhoto}/>
+            <div className="absolute bottom-0 h-1/3 bg-slate-500 bg-opacity-25 w-full text-center cursor-pointer">
+              <p className="text-sm p-1 text-white">upload</p>
+            </div>
+            <input
+              type={"file"}
+              id="profilePhoto"
+              className="hidden"
+              onChange={handleUploadProfilePhoto}
+            />
           </label>
         </div>
         {/**input form */}
-        <form className="w-full py-3 flex flex-col"onSubmit={handleSubmit}>
+        <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
           <label htmlFor="firstName">First Name</label>
           <input
             type={"text"}
@@ -182,4 +192,4 @@ const Signup = () => {
   );
 };
 
-export default Signup
+export default Signup;
