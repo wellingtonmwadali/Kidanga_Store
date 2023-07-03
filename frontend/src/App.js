5ee69c7271fae3b1af6b5835c0b92ab97e960dc6
Mwadali <wellingtonmwadali@gmail.com>
2023-07-03 22:12:59 +0300

    created new database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,26 @@ import Header from './component/header.js';
 import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 
+
+
 function App() {
   //fetch products
   useEffect(()=>{
-    (async()=>{
-      const res = await fetch(`$process.env.REACT_APP_SERVER_DOMAIN`)
-      const resData = await res.json()
-      console.log(resData)
-    })()
-  },[])
+    (async ()=>{
+      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
+      const resData = await res.json();
+      console.log(resData);
+    })();
+  }, []);
   return (
     <>
-     <Toaster />
-    <div className = ''>
-      <Header/>
-      <main className='pt-14 bg-slate-100 min-h-[calc(100vh)]'>
-        <Outlet />
+      <Toaster />
+      <div className="">
+        <Header />
+        <main className="pt-14 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
         </main>
-    </div>
+      </div>
     </>
   );
 }
